aa1e53d2868c1811b692b58caa342895c203f7c6
fuyubear <19767728+fuyubear@users.noreply.github.com>
2023-09-05 02:19:52 -0400

    Release resource lock slash command
    Intended to be a tool to temporarily fix stuck locks. Frequent use of this command suggests an issue with the locking mechanism.

---
+++ commands/release_release_lock.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { unlink } = require('fs').promises;
+
+const { parentLogger } = require('../logger');
+const logger = parentLogger.child({ module: 'commands-config_invite' });
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('resource_lock_release')
+        .setDescription('Release a resource lock with a specific name.')
+        .addStringOption(option =>
+            option.setName('resource_lock_name')
+                .setDescription('The name of the resource lock to release.')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('resource_lock_arg1')
+                .setDescription('First argument specifically describing the resource lock.')
+                .setRequired(false)),
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        if (!interaction.options.getString('resource_lock_name')) {
+            await interaction.editReply('You did not provide the resource lock name.')
+                .catch(err => logger.error(err));
+            return;
+        }
+
+        let lockName = 'lock-' + interaction.options.getString('resource_lock_name') + '-' + interaction.guild.id;
+
+        if (interaction.options.getString('resource_lock_arg1')) {
+            lockName += '-' + interaction.options.getString('resource_lock_arg1');
+        }
+
+        let unlockSuccess = true;
+        await unlink(lockName).catch((err) => {
+            unlockSuccess = false;
+            logger.error(err);
+        });
+
+        if (unlockSuccess) {
+            await interaction.editReply('I successfully released this resource lock.')
+                .catch(err => logger.error(err));
+        }
+        else {
+            await interaction.editReply('I failed to release this resource lock.')
+                .catch(err => logger.error(err));
+        }
+    },
+};
