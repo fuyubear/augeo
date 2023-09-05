a92bb5ec41346229ef7fde65a41a4d31394d0c83
fuyubear <19767728+fuyubear@users.noreply.github.com>
2023-09-05 02:21:59 -0400

    Invite slash command updates
    - Introduces use limits and per-use reset timings.
    - Configurable per guild: invite expiration, reason requirement, and use limits with per-use reset timings.

---
+++ commands/config_invite.js
@@ -0,0 +1,81 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { keyv } = require('../index');
+
+const { parentLogger } = require('../logger');
+const logger = parentLogger.child({ module: 'commands-config_invite' });
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('config_invite')
+        .setDescription('Configure the invite slash command.')
+        .addIntegerOption(option =>
+            option.setName('expire_after')
+                .setDescription('Configure the expiration time for newly created invites in seconds. Set to 0 for no expiration.')
+                .setRequired(true))
+        .addBooleanOption(option =>
+            option.setName('require_reason')
+                .setDescription('Configure if a reason is required to be specified when creating an invite.')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('use_limit')
+                .setDescription('Configure the use limit of the invite slash command per user. Set to 0 for unlimited uses.')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('use_expiration')
+                .setDescription('Configure how long it takes in seconds for a use to not count towards the use limit.')
+                .setRequired(false)),
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        const expireAfter = interaction.options.getInteger('expire_after');
+        const requireReason = interaction.options.getBoolean('require_reason');
+        const useLimit = interaction.options.getInteger('use_limit');
+        const useExpiration = interaction.options.getInteger('use_expiration');
+
+        if (!expireAfter || !useLimit || requireReason == null) {
+            await interaction.editReply('Not all config options were not provided.')
+                .catch(err => logger.error(err));
+            return;
+        }
+
+        if (!useLimit && useExpiration) {
+            await interaction.editReply('Cannot set a use expiration when there is no use limit.')
+                .catch(err => logger.error(err));
+            return;
+        }
+
+        if (useLimit > 0 && !useExpiration) {
+            await interaction.editReply('Must set a use expiration when there is a use limit.')
+                .catch(err => logger.error(err));
+            return;
+        }
+
+        const USER_INVITE_KEY_URL = `user-invite/${interaction.guildId}/settings`;
+        const userInviteSettingsToSave = {
+            expireAfter: expireAfter,
+            requireReason: requireReason,
+            useLimit: useLimit,
+            useExpiration: useExpiration,
+        };
+        await keyv.set(USER_INVITE_KEY_URL, userInviteSettingsToSave);
+
+        let newUserInviteSettings;
+        await keyv.get(USER_INVITE_KEY_URL)
+            .then(ret => newUserInviteSettings = ret)
+            .catch(err => logger.error(err));
+
+        if (!newUserInviteSettings
+            || userInviteSettingsToSave.expireAfter !== newUserInviteSettings.expireAfter
+            || userInviteSettingsToSave.useLimit !== newUserInviteSettings.useLimit
+            || userInviteSettingsToSave.useExpiration !== newUserInviteSettings.useExpiration
+            || userInviteSettingsToSave.requireReason !== newUserInviteSettings.requireReason
+        ) {
+            await interaction.editReply('Failed to save this invite slash command configuration.')
+                .catch(err => logger.error(err));
+        }
+        else {
+            await interaction.editReply('Successfully saved this invite slash command configuration.')
+                .catch(err => logger.error(err));
+        }
+    },
+};
--- commands/invite.js
+++ commands/invite.js
@@ -1,21 +1,92 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { existsSync, appendFile } = require('fs');
+const { unlink } = require('fs').promises;
 
+const { keyv } = require('../index');
 const { parentLogger } = require('../logger');
 const logger = parentLogger.child({ module: 'commands-invite' });
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('invite')
-        .setDescription('Create a single-use server invite that lasts for 7 days.')
+        .setDescription('Create a single-use server invite.')
         .addStringOption(option =>
             option.setName('reason')
-                .setDescription('Optionally add a reason for creating this server invite')
+                .setDescription('Add a reason for creating this server invite. May be required.')
                 .setRequired(false)),
     async execute(interaction) {
         await interaction.deferReply();
 
-        let reason = interaction.options.getString('reason');
         const channel = interaction.channel;
+        const timestamp = Math.floor(Date.now() / 1000);
+
+        let userInviteGuildSettings;
+        const USER_INVITE_GUILD_KEY_URL = `user-invite/${interaction.guildId}/settings`;
+        await keyv.get(USER_INVITE_GUILD_KEY_URL)
+            .then(ret => userInviteGuildSettings = ret)
+            .catch(err => logger.error(err));
+
+        if (!userInviteGuildSettings) {
+            await interaction.editReply('The config_invite slash command must be run before this command can be used. Contact your bot admin to fix this.')
+                .catch(err => logger.error(err));
+            return;
+        }
+
+        const instanceLogPrefix = 'Invite Run ID [' + interaction.guildId + '-' + interaction.member.id + '-' + timestamp + ']';
+
+        logger.info(instanceLogPrefix + ' Starting Invite processing.');
+
+        const lockFileName = 'lock-invite-' + interaction.guildId + '-' + interaction.member.id;
+
+        // Only 1 request should be processed at any time per guild and user.
+        let acquiredLock = false;
+        while (!acquiredLock) {
+            await getLock(lockFileName, instanceLogPrefix)
+                .then(val => acquiredLock = val)
+                .catch(err => logger.error(err));
+        }
+
+        let userInviteUserState;
+        const USER_INVITE_USER_STATE_KEY_URL = `user-invite/${interaction.guildId}/user/${interaction.member.id}`;
+        await keyv.get(USER_INVITE_USER_STATE_KEY_URL)
+            .then(ret => userInviteUserState = ret)
+            .catch(err => logger.error(err));
+
+        if (!userInviteUserState) {
+            userInviteUserState = {
+                uses: [],
+            };
+        }
+
+        // Update user state
+        for (let i = 0; i < userInviteUserState.uses.length; i++) {
+            if (userInviteUserState.uses[i] + userInviteGuildSettings.regenerateTime <= timestamp) {
+                userInviteUserState.uses.splice(i, 1);
+                i--;
+            }
+        }
+        await keyv.set(USER_INVITE_USER_STATE_KEY_URL, userInviteUserState).catch(err => logger.error(err));
+
+        // Make sure invites are available for use.
+        if (userInviteGuildSettings.useLimit > 0 && userInviteUserState.uses.length >= userInviteGuildSettings.useLimit) {
+            const secondsUntilNextUse = userInviteUserState.uses[0] + userInviteGuildSettings.regenerateTime - timestamp;
+            await interaction.editReply('You have used your entire invite creation allotment. You must wait '
+                + secondsUntilNextUse + ' seconds before you can create another invite or ask someone else to create one for you.')
+                .catch(err => logger.error(err));
+            await unlink(lockFileName).catch(err => logger.error(err));
+            logger.info(instanceLogPrefix + ' Ending Invite processing.');
+            return;
+        }
+
+
+        let reason = interaction.options.getString('reason');
+        if (userInviteGuildSettings.requireReason && reason == null) {
+            await interaction.editReply('You must provide the reason you\'re creating the invite.')
+                .catch(err => logger.error(err));
+            await unlink(lockFileName).catch(err => logger.error(err));
+            logger.info(instanceLogPrefix + ' Ending Invite processing.');
+            return;
+        }
 
         if (reason) {
             reason = `User ${interaction.user.id} created this invite: ` + reason;
@@ -26,7 +97,7 @@ module.exports = {
 
         let inviteSuccess = true;
         let inviteObj;
-        await channel.createInvite({ maxAge: 604800, maxUses: 1, unique: true, reason: reason })
+        await channel.createInvite({ maxAge: userInviteGuildSettings.expireAfter, maxUses: 1, unique: true, reason: reason })
             .then(invite => inviteObj = invite)
             .catch((err) => {
                 inviteSuccess = false;
@@ -37,8 +108,13 @@ module.exports = {
             // const msg = `A single-use 7 day server invite ${inviteObj.code} was created by user ${interaction.user.id}. Audit log reason: "${reason}"`;
             // PRIVACY: logger.info(msg);
             await interaction.editReply('I\'ve successfully created a single-use '
-                + `7 day server invite with invite code: ${inviteObj.code}`)
+                + `server invite with invite code: ${inviteObj.code}. This code expires in ${userInviteGuildSettings.expireAfter} seconds.`)
                 .catch(err => logger.error(err));
+
+            // Count the invite use and then save it.
+            // Should ensure the array is ordered from lowest (earliest) use to highest (latest) use.
+            userInviteUserState.uses.push(timestamp);
+            await keyv.set(USER_INVITE_USER_STATE_KEY_URL, userInviteUserState).catch(err => logger.error(err));
         }
         else {
             const msg = 'I\'ve failed to create an invite. Please make sure I have the permissions to create an invite.';
@@ -46,6 +122,22 @@ module.exports = {
             await interaction.editReply(msg).catch(err => logger.error(err));
         }
 
+        await unlink(lockFileName).catch(err => logger.error(err));
+        logger.info(instanceLogPrefix + ' Ending Invite processing.');
         return;
     },
 };
+
+async function getLock(lockName, instanceLogPrefix) {
+    if (existsSync(lockName)) {
+        logger.info(instanceLogPrefix + ' Waiting 1 second for lock ' + lockName);
+        await new Promise(r => setTimeout(r, 1000));
+        return false;
+    }
+    else {
+        appendFile(lockName, lockName, function(err) {
+            if (err) return err;
+        });
+        return true;
+    }
+}
